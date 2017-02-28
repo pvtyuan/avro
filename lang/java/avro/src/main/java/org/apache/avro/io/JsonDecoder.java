2723af762f8129d0fc94d7c5699733f0c3b5166b
pvtyuan <pvtyuan@foxmail.com>
2017-02-28 11:23:43 +0800

    switch to java.util.Base64

--- lang/java/avro/src/main/java/org/apache/avro/io/JsonDecoder.java
+++ lang/java/avro/src/main/java/org/apache/avro/io/JsonDecoder.java
@@ -286,7 +286,7 @@ public class JsonDecoder extends ParsingDecoder
               }
               break;
           case BASE64:
-              result = org.apache.commons.codec.binary.Base64.decodeBase64(text);
+              result = Base64.getDecoder().decode(text);
               break;
       }
       return result;
